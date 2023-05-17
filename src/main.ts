dd904f539b64e6a6d89ef03178f2fcf2517f81a4
ivy <ivy@not-ivy.dev>
2023-05-17 16:02:46 -0700

    move html out of ts

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,12 @@ import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 const app = new Hono();
 
 app.get("/", (ctx) => {
-  return ctx.html(`
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
-  <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-  <form action="/dl" method="GET" style="padding: 1.5rem" class="pure-form">
-    <fieldset>
-      <legend>simple ytdl-core frontend built with deno + hono + purecss</legend>
-      <label for="url">URL</label>
-      <input type="text" id="url" name="url" placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="pure-input-1-4" required />
-      <button type="submit" class="pure-button pure-button-primary">Download</button>
-      <input type="hidden" id="ip" name="ip" />
-      <span class="pure-form-message">Your IP will be collected for verification purposes</span>
-      <div class="cf-turnstile" data-sitekey="${Deno.env.get(
-        "TURNSTILE_SITE_KEY"
-      )}" style="margin-top: 2rem;"></div>
-    </fieldset>
-    <script>
-      document.querySelector("form").addEventListener("submit", (e) => {
-        e.preventDefault();
-        fetch("https://ifconfig.me/ip").then((res) => res.text()).then((ip) => {
-          document.querySelector("#ip").value = ip;
-        }).then(() => {
-          document.querySelector("form").submit();
-        });
-      });
-    </script>
-  </form>
-  `);
+  return ctx.html(
+    Deno.readTextFileSync("./index.html").replace(
+      "$TURNSTILE_SITE_KEY",
+      Deno.env.get("TURNSTILE_SITE_KEY") || ""
+    )
+  );
 });
 
 app.get("/dl", async (ctx) => {
