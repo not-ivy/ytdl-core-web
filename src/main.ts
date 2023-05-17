e8097b2ebb42eaab3da158079b23b3379f456adc
ivy <ivy@not-ivy.dev>
2023-05-17 16:44:40 -0700

    form validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "https://deno.land/std@0.187.0/dotenv/load.ts";
 import { Hono } from "https://deno.land/x/hono@v3.2.0-rc.3/mod.ts";
 import { serve } from "https://deno.land/std@0.187.0/http/server.ts";
 import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
+import * as $ from "https://deno.land/x/scale@v0.11.2/mod.ts";
 
 const app = new Hono();
 
@@ -14,37 +15,39 @@ app.get("/", (ctx) => {
   );
 });
 
-app.get("/dl", async (ctx) => {
-  const url = ctx.req.query("url");
-  if (!url) return ctx.text("No url provided");
-  const cfTurnstileResponse = ctx.req.query("cf-turnstile-response");
-  if (!cfTurnstileResponse)
-    return ctx.text("No cf-turnstile-response provided");
-  const ip = ctx.req.query("ip");
+const $dlFormData = $.object(
+  $.field("url", $.str),
+  $.field("cf-turnstile-response", $.str),
+  $.optionalField("ip", $.str)
+);
+
+app.post("/dl", async (ctx) => {
+  const dlFormData = Object.fromEntries(await ctx.req.formData());
+
+  if (!$.is($dlFormData, dlFormData)) {
+    return ctx.text("Invalid form data");
+  }
 
   const formData = new FormData();
   formData.append("secret", Deno.env.get("TURNSTILE_SECRET_KEY")!);
-  formData.append("response", cfTurnstileResponse);
-  ip ? formData.append("ip", ip) : null;
+  formData.append("response", dlFormData["cf-turnstile-response"]);
+  formData.append("ip", dlFormData["ip"] || "");
 
-  const turnstileRes = await (
+  const turnstileRes: { success: boolean } = await (
     await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
       body: formData,
       method: "POST",
     })
   ).json();
 
-  if (!turnstileRes.success) return ctx.text("Turnstile verification failed");
-
-  console.log(`${ip} is downloading ${url}`);
-
-  const stream = await ytdl(url, {
+  const stream = await ytdl("https://www.youtube.com/watch?v=FjCVsnYbS58", {
     quality: "highestaudio",
   });
 
-  return new Response(stream, {
+  return ctx.newResponse(stream, {
     headers: {
       "Content-Type": "video/mp4",
+      "Content-Disposition": `attachment; filename="${stream.info.videoDetails.videoId}.mp4"`,
     },
   });
 });
