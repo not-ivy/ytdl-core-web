f73d12d52e677bb195b570df6f00a5d8f654e00e
ivy <ivy@not-ivy.dev>
2023-05-18 09:16:49 -0700

    fix html display

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,32 @@
 import "https://deno.land/std@0.187.0/dotenv/load.ts";
-import cheetah from "https://deno.land/x/cheetah@v0.7.1/mod.ts";
+import cheetah from "https://deno.land/x/cheetah@v0.7.2/mod.ts";
 import { serve } from "https://deno.land/std@0.187.0/http/server.ts";
 import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
-import zod, { z } from 'https://deno.land/x/cheetah@v0.7.1/validator/zod.ts'
+import * as $ from "https://deno.land/x/scale@v0.11.2/mod.ts";
 
-const app = new cheetah({ validator: zod });
+const app = new cheetah();
 
 app.get("/", (ctx) => {
   ctx.res.header("Content-Type", "text/html");
-  ctx.res.text(Deno.readTextFileSync("./index.html").replace("$TURNSTILE_SITE_KEY", Deno.env.get("TURNSTILE_SITE_KEY") || ""));
+  return Deno.readTextFileSync("./index.html").replace("$TURNSTILE_SITE_KEY", Deno.env.get("TURNSTILE_SITE_KEY") || "");
 });
 
-app.post("/dl", {
-  body: z.object({
-    url: z.string(),
-    "cf-turnstile-response": z.string(),
-  })
-}, async (ctx) => {
+const $dlFormData = $.object(
+  $.field("url", $.str),
+  $.field("cf-turnstile-response", $.str),
+);
+
+app.post("/dl", async (ctx) => {
+  const dlFormData = Object.fromEntries(await ctx.req.formData() || []);
+
+  if (!$.is($dlFormData, dlFormData)) {
+    ctx.res.code(400);
+    return "Invalid form data";
+  }
+
   const formData = new FormData();
   formData.append("secret", Deno.env.get("TURNSTILE_SECRET_KEY")!);
-  formData.append("response", ctx.req.body["cf-turnstile-response"]);
+  formData.append("response", dlFormData["cf-turnstile-response"]);
   formData.append("ip", ctx.req.ip || "");
 
   const turnstileRes: { success: boolean } = await (
@@ -29,21 +36,18 @@ app.post("/dl", {
     })
   ).json();
 
-  console.log("Verified turnstile");
-
   if (!turnstileRes.success) {
-    ctx.res.text("Turnstile verification failed");
+    ctx.res.code(403);
+    return "Turnstile verification failed";
   }
 
-  const stream = await ytdl(ctx.req.body.url, {
+  const stream = await ytdl(dlFormData.url, {
     quality: "highestaudio",
   });
 
-  console.log("Downloading video");
-
   ctx.res.header("Content-Type", "video/mp4");
   ctx.res.header("Content-Disposition", `attachment; filename="${stream.info.videoDetails.videoId}.mp4"`);
-  ctx.res.stream(stream)
+  return ctx.res.stream(stream)
 });
 
 serve(app.fetch);
